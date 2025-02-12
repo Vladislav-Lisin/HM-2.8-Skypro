ca2117f12b82026ffde0635027d2466d07d561e0
Vladislav <M1LkMenIO@yandex.ru>
2025-02-12 21:06:53 +0300

    HM-2.8.1

---
+++ src/main/java/pro/sky/coursework/EmployeeService.java
@@ -0,0 +1,44 @@
+package pro.sky.coursework;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private static final int maxCountEmployees = 10;
+    List<Employee> employeesList = new ArrayList<>();
+
+
+    public Employee addEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (employeesList.size() >= maxCountEmployees) {
+            throw new EmployeeStorageIsFullException("Достигнут лимит количества сотрудников в фирме.");
+        }
+        if (employeesList.contains(employee)) {
+            throw new EmployeeAlreadyAddedException("Сотрудник с таким именем уже существует.");
+        }
+        employeesList.add(employee);
+        return employee;
+    }
+
+    public Employee removeEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (!employeesList.contains(employee)) {
+            throw new EmployeeNotFoundException("Сотрудник с таким именем не найден.");
+        }
+        employeesList.remove(employee);
+        return employee;
+    }
+
+    public Employee findeEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        if (!employeesList.contains(employee)) {
+            throw new EmployeeNotFoundException("Сотрудник не найден.");
+        }
+        return employee;
+    }
+
+}
